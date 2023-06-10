673c0425bf0253cc198cff7e4f040d4f69b22418
Adrian Lazarec <lazarec.adrian@gmail.com>
2023-06-10 21:36:59 +0300

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { DeviceListComponent } from './device-list/device-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'devices', component: DeviceListComponent },
+  { path: '', redirectTo: 'devices', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/device-list/device-list.component.ts
+++ src/app/device-list/device-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-device-list',
   templateUrl: './device-list.component.html',
   styleUrls: ['./device-list.component.css']
 })
-export class DeviceListComponent {
+export class DeviceListComponent implements OnInit {
+  devices: any[] = []; // Property to hold the list of devices
+  error: string = ''; // Property to hold error messages, if any
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getDevices();
+    // Initialization logic here
+  }
+  getDevices() {
+    this.http.get<any[]>('https://localhost:7195/api/Device')
+      .subscribe(
+        response => {
+          this.devices = response; // Update the component property with the received data
+        },
+        error => {
+          this.error = 'Error fetching devices.'; // Handle and display the error, if any
+        }
+      );
+  }
 }
