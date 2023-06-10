87923c0c068e7f80e51f3c3561ddd375248ea847
Adrian Lazarec <lazarec.adrian@gmail.com>
2023-06-10 20:38:48 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeviceListComponent } from './device-list/device-list.component';
+import { DeviceDetailsComponent } from './device-details/device-details.component';
+import { DeviceCreateComponent } from './device-create/device-create.component';
+import { DeviceUpdateComponent } from './device-update/device-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceListComponent,
+    DeviceDetailsComponent,
+    DeviceCreateComponent,
+    DeviceUpdateComponent
   ],
   imports: [
     BrowserModule,
