8d5ed71f6dee4397ef9f157cfd80d6ed405c4cce
Adrian Lazarec <lazarec.adrian@gmail.com>
2023-06-11 10:09:39 +0300

    Created device-details. component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DeviceListComponent } from './device-list/device-list.component';
+import { DeviceDetailsComponent } from './device-details/device-details.component';
 
 const routes: Routes = [
-  {
-    path: 'devices', component: DeviceListComponent },
+  { path: 'devices', component: DeviceListComponent },
+  { path: 'devices/:id', component: DeviceDetailsComponent },
   { path: '', redirectTo: '', pathMatch: 'full' }
 ];
 
--- src/app/device-details/device-details.component.ts
+++ src/app/device-details/device-details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-device-details',
   templateUrl: './device-details.component.html',
   styleUrls: ['./device-details.component.css']
 })
-export class DeviceDetailsComponent {
+export class DeviceDetailsComponent implements OnInit {
+  deviceId: number = 0;
+  device: any;
+  error: string = '';
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.deviceId = +params['id']; // Extract the device ID from the route parameter
+      this.getDevice();
+    });
+  }
+
+  getDevice() {
+    this.http.get<any>(`https://localhost:7195/api/Device/${this.deviceId}`)
+      .subscribe(
+        response => {
+          this.device = response; // Update the component property with the received device data
+        },
+        error => {
+          this.error = 'Error fetching device details.'; // Handle and display the error, if any
+        }
+      );
+  }
 }
